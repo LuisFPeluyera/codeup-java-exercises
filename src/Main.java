60baa9c4509cec174593718d032528bee1f588b9
luisfpeluyera <luis.f.peluyera@gmail.com>
2023-09-27 16:56:53 -0500

    console exercises init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,104 @@
+import  java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hellooooooo world!");
+
+
+        int myFavoriteNumber = 7;
+
+        System.out.println(myFavoriteNumber);
+
+        myFavoriteNumber = '1';
+
+        System.out.println(myFavoriteNumber);
+
+        String myString = "This is a Java test";
+
+        System.out.println(myString);
+
+        myString = String.valueOf('a');
+
+        System.out.println(myString);
+
+        myString = String.valueOf(3.14159);
+
+        System.out.println(myString);
+
+        long myNumber = 0;
+
+        //float myNumber = (float) 3.14;
+
+        System.out.println();
+
+        System.out.println(myNumber);
+
+        myNumber = (long) 3.14159;
+
+        System.out.println(myNumber);
+
+        myNumber = 123L;
+
+        System.out.println(myNumber);
+
+        myNumber = 123;
+
+        System.out.println(myNumber);
+
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+        System.out.println();
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+        //String class = "a string";
+
+        String theNumberThree = "three";
+        System.out.println(theNumberThree);
+        Object o = theNumberThree;
+        System.out.println(o);
+       //int three = (int) o;
+
+       // int three = (int) "three";
+
+
+//        int y = 4;
+//       System.out.println(y = y + 5);
+//
+//       System.out.println(y += 5);
+
+//        int x = 3;
+//        int y = 4;
+//        System.out.println(y = y * x);
+
+//
+//        int x = 10;
+//        int y = 2;
+//        x = x / y;
+//        y = y - x;
+//
+//        System.out.println(x);
+//        System.out.println(y);
+
+
+
+Scanner myScanner = new Scanner(System.in);
+
+System.out.println("What kind of coffee should we get?");
+
+
+
+
+
+
+
     }
+
 }
+
+
